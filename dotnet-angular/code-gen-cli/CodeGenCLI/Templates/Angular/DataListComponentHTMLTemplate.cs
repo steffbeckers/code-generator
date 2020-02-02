c12bab83ea6ebbdf0f6a018de2c6871d1b071eb6
Steff Beckers <steff@steffbeckers.eu>
2020-02-02 20:28:13 +0100

    Save before generating Angular

--- dotnet-angular/code-gen-cli/CodeGenCLI/Templates/Angular/DataListComponentHTMLTemplate.cs
+++ dotnet-angular/code-gen-cli/CodeGenCLI/Templates/Angular/DataListComponentHTMLTemplate.cs
@@ -290,7 +290,7 @@ namespace CodeGenCLI.Templates.Angular
             this.Write(".");
             
             #line 39 "C:\dev\steffbeckers\code-generator\dotnet-angular\code-gen-cli\CodeGenCLI\Templates\Angular\DataListComponentHTMLTemplate.tt"
-            this.Write(this.ToStringHelper.ToStringWithCulture(relation.DisplayField.ToCamelCase()));
+            this.Write(this.ToStringHelper.ToStringWithCulture(!string.IsNullOrEmpty(model.DisplayField) ? model.DisplayField.ToCamelCase() : "id"));
             
             #line default
             #line hidden
