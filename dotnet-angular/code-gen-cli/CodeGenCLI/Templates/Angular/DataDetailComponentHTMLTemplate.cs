6703984f095c2f61906acf66d94facd099d9724e
Steff Beckers <steff@steffbeckers.eu>
2020-02-02 20:28:33 +0100

    Save before generating Angular

--- dotnet-angular/code-gen-cli/CodeGenCLI/Templates/Angular/DataDetailComponentHTMLTemplate.cs
+++ dotnet-angular/code-gen-cli/CodeGenCLI/Templates/Angular/DataDetailComponentHTMLTemplate.cs
@@ -232,7 +232,7 @@ namespace CodeGenCLI.Templates.Angular
             this.Write(".");
             
             #line 33 "C:\dev\steffbeckers\code-generator\dotnet-angular\code-gen-cli\CodeGenCLI\Templates\Angular\DataDetailComponentHTMLTemplate.tt"
-            this.Write(this.ToStringHelper.ToStringWithCulture(relation.DisplayField.ToCamelCase()));
+            this.Write(this.ToStringHelper.ToStringWithCulture(!string.IsNullOrEmpty(model.DisplayField) ? model.DisplayField.ToCamelCase() : "id"));
             
             #line default
             #line hidden
