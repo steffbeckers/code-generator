ba6f1984444069be37f1d4001c5f3b2b5a64913e
Steff Beckers <steff@steffbeckers.eu>
2020-02-09 17:41:42 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Models/User.cs
+++ dotnet-angular/test/web-api/Models/User.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Test.API.Models
 {
-    public class User : IdentityUser
+    public class User : IdentityUser<Guid>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
