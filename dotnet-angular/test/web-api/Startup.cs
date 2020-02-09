d3d6ca81b23ca912f18c1d00c86f8608eb166548
Steff Beckers <steff@steffbeckers.eu>
2020-02-09 17:13:31 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Startup.cs
+++ dotnet-angular/test/web-api/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.IO;
