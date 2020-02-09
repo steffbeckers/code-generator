05a2461f6fd50795202caa9c78ed678a543c0502
Steff Beckers <steff@steffbeckers.eu>
2020-02-09 14:14:01 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Services/EmailService.cs
+++ dotnet-angular/test/web-api/Services/EmailService.cs
@@ -60,5 +60,11 @@ namespace Test.API.Services
                 throw;
             }
         }
+
+        public async Task SendEmailConfirmationAsync(string email, string link)
+        {
+            return emailSender.SendEmailAsync(email, "Confirm your email",
+                $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
+        }
     }
 }
