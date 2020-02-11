bc2336afda18944faaf66df89cdc6a441cbef0e3
Steff Beckers <steff@steffbeckers.eu>
2020-02-11 22:40:06 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/BLL/AuthBLL.cs
+++ dotnet-angular/test/web-api/BLL/AuthBLL.cs
@@ -11,5 +11,18 @@ namespace Test.API.BLL
 	/// </summary>
     public class AuthBLL
     {
+        private string GenerateJWT(List<Claim> claims)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(configuration.GetSection("Authentication").GetValue<string>("Secret"));
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(double.Parse(configuration.GetSection("Authentication").GetValue<string>("TokenExpiresInMinutes"))),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+        }
     }
 }
