a705f62ae273ccec8817cd5f64b8dc2bbbbf5aca
Steff Beckers <steff@steffbeckers.eu>
2020-02-11 22:33:25 +0100

    Save before generating Web API

---
+++ dotnet-angular/test/web-api/BLL/AuthBLL.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Test.API.DAL.Repositories;
+using Test.API.Models;
+
+namespace Test.API.BLL
+{
+	/// <summary>
+	/// The business logic layer for authentication.
+	/// </summary>
+    public class AuthBLL
+    {
+    }
+}
