2f64ecffe1ba89ac2e39d68d350f6222514c2049
Steff Beckers <steff@steffbeckers.eu>
2020-02-11 22:49:57 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Controllers/AuthController.cs
+++ dotnet-angular/test/web-api/Controllers/AuthController.cs
@@ -7,13 +7,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 using Test.API.BLL;
 
