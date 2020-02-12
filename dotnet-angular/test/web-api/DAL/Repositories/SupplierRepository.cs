581718df1956ef066f5a6067a68e13d517e5bb61
Steff Beckers <steff@steffbeckers.eu>
2020-02-12 22:22:21 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/DAL/Repositories/SupplierRepository.cs
+++ dotnet-angular/test/web-api/DAL/Repositories/SupplierRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Test.API.Framework;
 using Test.API.Models;
 
 namespace Test.API.DAL.Repositories
