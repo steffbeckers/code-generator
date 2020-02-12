8a4c20921ddaa3a0b314326cdb6ca1b3f5572a47
Steff Beckers <steff@steffbeckers.eu>
2020-02-12 22:14:35 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Framework/Repository.cs
+++ dotnet-angular/test/web-api/Framework/Repository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace Test.API.DAL
+namespace Test.API.Framework
 {
     interface IRepository<TEntity> where TEntity : class
     {
