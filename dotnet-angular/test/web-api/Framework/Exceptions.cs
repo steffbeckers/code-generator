c32b4be777b6c0ae39be24ef7656b5b734450099
Steff Beckers <steff@steffbeckers.eu>
2020-02-12 22:32:41 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Framework/Exceptions.cs
+++ dotnet-angular/test/web-api/Framework/Exceptions.cs
@@ -2,8 +2,12 @@ using System;
 
 namespace Test.API.Framework.Exceptions
 {
+    # region Authentication
+
     public class LoginFailedException : Exception
     {
         public LoginFailedException(string message) : base(message) {}
     }
+
+    #endregion
 }
