346327863b8eddab7855df186d1f30dda9601fb2
Steff Beckers <steff@steffbeckers.eu>
2020-02-15 18:54:02 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Controllers/AuthController.cs
+++ dotnet-angular/test/web-api/Controllers/AuthController.cs
@@ -93,119 +93,119 @@ namespace Test.API.Controllers
             return Ok();
         }
 
-        [HttpGet]
-        [Route("confirm-email")]
-        [AllowAnonymous]
-        public async Task<IActionResult> ConfirmEmail(string userId, string code)
-        {
-            if (userId == null || code == null)
-            {
-                return BadRequest(ModelState);
-            }
+        //[HttpGet]
+        //[Route("confirm-email")]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        //{
+        //    if (userId == null || code == null)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            var user = await userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                throw new ApplicationException($"Unable to load user with ID '{userId}'.");
-            }
+        //    var user = await userManager.FindByIdAsync(userId);
+        //    if (user == null)
+        //    {
+        //        throw new ApplicationException($"Unable to load user with ID '{userId}'.");
+        //    }
 
-            var result = await userManager.ConfirmEmailAsync(user, code);
-            if (result.Succeeded)
-            {
-                return Ok();
-            }
+        //    var result = await userManager.ConfirmEmailAsync(user, code);
+        //    if (result.Succeeded)
+        //    {
+        //        return Ok();
+        //    }
 
-            // If we got this far, something failed
-            return BadRequest(ModelState);
-        }
+        //    // If we got this far, something failed
+        //    return BadRequest(ModelState);
+        //}
 
-        [HttpPost]
-        [Route("forgot-password")]
-        [AllowAnonymous]
-        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordVM model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = await userManager.FindByEmailAsync(model.Email);
-                if (user == null)
-                {
-                    //// Don't reveal that the user does not exist
-                    //return Ok();
+        //[HttpPost]
+        //[Route("forgot-password")]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordVM model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = await userManager.FindByEmailAsync(model.Email);
+        //        if (user == null)
+        //        {
+        //            //// Don't reveal that the user does not exist
+        //            //return Ok();
 
-                    // For CRM purposes
-                    return NotFound("email-not-found");
-                }
+        //            // For CRM purposes
+        //            return NotFound("email-not-found");
+        //        }
 
-                // Check if email is confirmed, if required in Startup settings
-                // In startup: options.SignIn.RequireConfirmedEmail = true;
-                if (!(await userManager.IsEmailConfirmedAsync(user)))
-                {
-                    //// Don't reveal that the user does not exist
-                    //return Ok();
+        //        // Check if email is confirmed, if required in Startup settings
+        //        // In startup: options.SignIn.RequireConfirmedEmail = true;
+        //        if (!(await userManager.IsEmailConfirmedAsync(user)))
+        //        {
+        //            //// Don't reveal that the user does not exist
+        //            //return Ok();
 
-                    // OR
+        //            // OR
 
-                    return NotFound("email-not-confirmed");
-                }
+        //            return NotFound("email-not-confirmed");
+        //        }
 
-                // For more information on how to enable account confirmation and password reset please
-                // visit https://go.microsoft.com/fwlink/?LinkID=532713
+        //        // For more information on how to enable account confirmation and password reset please
+        //        // visit https://go.microsoft.com/fwlink/?LinkID=532713
 
-                string code = await userManager.GeneratePasswordResetTokenAsync(user);
+        //        string code = await userManager.GeneratePasswordResetTokenAsync(user);
 
-                var callbackUrl = configuration.GetSection("EmailSettings").GetValue<string>("PasswordResetURL");
-                callbackUrl = callbackUrl.Replace("{{userId}}", user.Id.ToString().ToLower());
-                callbackUrl = callbackUrl.Replace("{{userEmail}}", user.Email.ToString().ToLower());
-                callbackUrl = callbackUrl.Replace("{{code}}", Uri.EscapeDataString(code));
+        //        var callbackUrl = configuration.GetSection("EmailSettings").GetValue<string>("PasswordResetURL");
+        //        callbackUrl = callbackUrl.Replace("{{userId}}", user.Id.ToString().ToLower());
+        //        callbackUrl = callbackUrl.Replace("{{userEmail}}", user.Email.ToString().ToLower());
+        //        callbackUrl = callbackUrl.Replace("{{code}}", Uri.EscapeDataString(code));
 
-                await emailService.SendEmailAsync(model.Email, "Reset Password",
-                   $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
+        //        await emailService.SendEmailAsync(model.Email, "Reset Password",
+        //           $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
 
-                return Ok();
-            }
+        //        return Ok();
+        //    }
 
-            // If we got this far, something failed
-            return BadRequest(ModelState);
-        }
+        //    // If we got this far, something failed
+        //    return BadRequest(ModelState);
+        //}
 
-        [HttpPost]
-        [Route("reset-password")]
-        [AllowAnonymous]
-        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordVM model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = await userManager.FindByIdAsync(model.Id);
-                if (user == null)
-                {
-                    //return BadRequest();
+        //[HttpPost]
+        //[Route("reset-password")]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordVM model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = await userManager.FindByIdAsync(model.Id);
+        //        if (user == null)
+        //        {
+        //            //return BadRequest();
 
-                    // OR
+        //            // OR
 
-                    // For CRM purposes
-                    return NotFound("user-not-found");
-                }
-                if (user.Email != model.Email)
-                {
-                    //return BadRequest();
+        //            // For CRM purposes
+        //            return NotFound("user-not-found");
+        //        }
+        //        if (user.Email != model.Email)
+        //        {
+        //            //return BadRequest();
 
-                    // OR
+        //            // OR
 
-                    // For CRM purposes
-                    return BadRequest("email-does-not-match");
-                }
+        //            // For CRM purposes
+        //            return BadRequest("email-does-not-match");
+        //        }
 
-                var result = await userManager.ResetPasswordAsync(user, model.Code, model.Password);
-                if (result.Succeeded)
-                {
-                    // TODO: Maybe log the user in automatically? Need to add/refactor JWT logic if adding
-                    //await signInManager.SignInAsync(user, isPersistent: false);
+        //        var result = await userManager.ResetPasswordAsync(user, model.Code, model.Password);
+        //        if (result.Succeeded)
+        //        {
+        //            // TODO: Maybe log the user in automatically? Need to add/refactor JWT logic if adding
+        //            //await signInManager.SignInAsync(user, isPersistent: false);
 
-                    return Ok();
-                }
-            }
+        //            return Ok();
+        //        }
+        //    }
 
-            return BadRequest(ModelState);
-        }
+        //    return BadRequest(ModelState);
+        //}
     }
 }
