06e3626c8f358b345f7bcdd5b615784b904ecf13
Steff Beckers <steff@steffbeckers.eu>
2020-02-15 17:17:25 +0100

    Save before generating Web API

---
+++ dotnet-angular/test/web-api/Migrations/20200215161545_Generatedb69212732f4f48eb8cfb49c050aadf7f.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Test.API.Migrations
+{
+    public partial class Generatedb69212732f4f48eb8cfb49c050aadf7f : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
