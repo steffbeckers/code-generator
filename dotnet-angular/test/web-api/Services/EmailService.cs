bd772b64cd2a2582b0146f11a94797be865d2b72
Steff Beckers <steff@steffbeckers.eu>
2020-02-16 15:36:22 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Services/EmailService.cs
+++ dotnet-angular/test/web-api/Services/EmailService.cs
@@ -65,7 +65,13 @@ namespace Test.API.Services
         public Task SendEmailConfirmationAsync(string email, string link)
         {
             return SendEmailAsync(email, "Confirm your email",
-                $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
+                $"<p>Please confirm your account by clicking this link:</p><br /><a href='{HtmlEncoder.Default.Encode(link)}'>{link}</a>");
+        }
+
+        public Task SendPasswordResetAsync(string email, string link)
+        {
+            return SendEmailAsync(email, "Reset your password",
+                $"<p>Please reset your password by clicking this link:</p><br /><a href='{HtmlEncoder.Default.Encode(link)}'>{link}</a>");
         }
     }
 }
