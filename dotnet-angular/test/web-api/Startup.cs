e671781a605bb1cab62dbf7a5f878c8ebeaac064
Steff Beckers <steff@steffbeckers.eu>
2020-02-25 23:19:47 +0100

    Save before generating Web API

--- dotnet-angular/test/web-api/Startup.cs
+++ dotnet-angular/test/web-api/Startup.cs
@@ -226,7 +226,7 @@ namespace Test.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
 		    // CORS
             app.UseCors(options =>
