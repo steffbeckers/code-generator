28f15d3867257bd2f955dfe7dbbbcd0c7cef6493
Steff Beckers <steff@steffbeckers.eu>
2020-03-01 21:42:20 +0100

    Prog update

--- dotnet-angular/code-gen-cli/CodeGenCLI/Program.cs
+++ dotnet-angular/code-gen-cli/CodeGenCLI/Program.cs
@@ -550,24 +550,50 @@ namespace CodeGenCLI
                     gitStatus.WorkingDirectory = Config.WebAPI.ProjectPath;
                     Process.Start(gitStatus).WaitForExit();
 
-                    //Console.WriteLine();
-                    //Console.WriteLine("### git checkout -p ###");
+                    Console.WriteLine();
+                    Console.WriteLine("### git checkout -p ###");
 
-                    //Process gitCheckoutP = new Process
-                    //{
-                    //    StartInfo = new ProcessStartInfo
-                    //    {
-                    //        FileName = "git",
-                    //        Arguments = "checkout -p",
-                    //        WorkingDirectory = Config.WebAPI.ProjectPath,
-                    //        RedirectStandardOutput = false,
-                    //        RedirectStandardInput = false,
-                    //        CreateNoWindow = true,
-                    //        UseShellExecute = false
-                    //    }
-                    //};
+                    Process gitCheckoutP = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "git",
+                            Arguments = "checkout -p",
+                            WorkingDirectory = Config.WebAPI.ProjectPath,
+                            RedirectStandardOutput = true,
+                            RedirectStandardInput = true,
+                            CreateNoWindow = true,
+                            UseShellExecute = false
+                        }
+                    };
 
-                    //gitCheckoutP.Start();
+                    gitCheckoutP.Start();
+
+                    string line;
+                    string currentHunk = string.Empty;
+
+                    while ((line = gitCheckoutP.StandardOutput.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+
+                        if (line.Equals("Discard this hunk from worktree [y,n,q,a,d,e,?]? "))
+                        {
+                            if (currentHunk.Contains("#-#-#"))
+                            {
+                                gitCheckoutP.StandardInput.WriteLine("y");
+                                currentHunk = string.Empty;
+                            }
+                            else
+                            {
+                                gitCheckoutP.StandardInput.WriteLine("n");
+                                currentHunk = string.Empty;
+                            }
+                        }
+                        else
+                        {
+                            currentHunk += line + Environment.NewLine;
+                        }
+                    }
 
                     //string output = gitCheckoutP.StandardOutput.ReadToEnd();
 
@@ -621,7 +647,8 @@ namespace CodeGenCLI
                     //}
 
                     //watchingOutput = false;
-                    //gitCheckoutP.WaitForExit();
+                    
+                    gitCheckoutP.WaitForExit();
 
                     #endregion;
 
