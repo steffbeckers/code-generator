956d3c25c75fcc7f8c0b47e0558630edb0079636
Steff Beckers <steff@steffbeckers.eu>
2020-03-01 21:04:30 +0100

    Prog update

--- dotnet-angular/code-gen-cli/CodeGenCLI/Program.cs
+++ dotnet-angular/code-gen-cli/CodeGenCLI/Program.cs
@@ -561,7 +561,7 @@ namespace CodeGenCLI
                             Arguments = "checkout -p",
                             WorkingDirectory = Config.WebAPI.ProjectPath,
                             RedirectStandardOutput = true,
-                            RedirectStandardInput = false,
+                            RedirectStandardInput = true,
                             CreateNoWindow = true,
                             UseShellExecute = false
                         }
