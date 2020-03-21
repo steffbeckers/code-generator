f5343c6f3f57646709ed9af05d5b1b08e32c7987
Steff Beckers <steff@steffbeckers.eu>
2020-03-21 01:06:19 +0100

    Save before generating Web API

---
+++ dotnet-angular/test/web-api/Migrations/20200321000421_ResumesAndSkills.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RJM.API.Migrations
+{
+    public partial class ResumesAndSkills : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Resumes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    JobTitle = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    CreatedByUserId = table.Column<Guid>(nullable: false),
+                    ModifiedByUserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resumes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Resumes_Users_CreatedByUserId",
+                        column: x => x.CreatedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Resumes_Users_ModifiedByUserId",
+                        column: x => x.ModifiedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResumeSkill",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Rating = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    ResumeId = table.Column<Guid>(nullable: false),
+                    SkillId = table.Column<Guid>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    CreatedByUserId = table.Column<Guid>(nullable: false),
+                    ModifiedByUserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResumeSkill", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResumeSkill_Users_CreatedByUserId",
+                        column: x => x.CreatedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ResumeSkill_Users_ModifiedByUserId",
+                        column: x => x.ModifiedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ResumeSkill_Resumes_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resumes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResumeSkill_Skills_SkillId",
+                        column: x => x.SkillId,
+                        principalTable: "Skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resumes_CreatedByUserId",
+                table: "Resumes",
+                column: "CreatedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resumes_ModifiedByUserId",
+                table: "Resumes",
+                column: "ModifiedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResumeSkill_CreatedByUserId",
+                table: "ResumeSkill",
+                column: "CreatedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResumeSkill_ModifiedByUserId",
+                table: "ResumeSkill",
+                column: "ModifiedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResumeSkill_ResumeId",
+                table: "ResumeSkill",
+                column: "ResumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResumeSkill_SkillId",
+                table: "ResumeSkill",
+                column: "SkillId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ResumeSkill");
+
+            migrationBuilder.DropTable(
+                name: "Resumes");
+        }
+    }
+}
