fb937c9674224ff0c5cc8ba3f593d7e1f8171972
Steff Beckers <steff@steffbeckers.eu>
2020-06-16 21:04:33 +0200

    Test 2 added

--- dotnet-angular/test/web-api/BLL/AuthBLL.cs
+++ dotnet-angular/test/web-api/BLL/AuthBLL.cs
@@ -52,6 +52,8 @@ namespace RJM.API.BLL
             this.emailService = emailService;
         }
 
+        // Test 2
+
         public async Task<AuthenticatedVM> Login(LoginVM loginVM)
         {
             // Validation
