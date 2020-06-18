a04049cadb867427b67b711406fdf8f68daf58c9
Steff Beckers <steff@steffbeckers.eu>
2020-06-19 00:55:48 +0200

    Save before generating Web API

--- dotnet-angular/test/web-api/BLL/AuthBLL.cs
+++ dotnet-angular/test/web-api/BLL/AuthBLL.cs
@@ -53,8 +53,6 @@ namespace RJM.API.BLL
             this.emailService = emailService;
         }
 
-        // Test 2
-
         public async Task<AuthenticatedVM> Login(LoginVM loginVM)
         {
             // Validation
