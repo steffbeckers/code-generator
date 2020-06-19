29257a60796e6d236ddaca766ea04500c3e26f3f
Steff Beckers <steff@steffbeckers.eu>
2020-06-19 20:56:02 +0200

    Removed @@@

--- dotnet-angular/test/web-api/Controllers/AuthController.cs
+++ dotnet-angular/test/web-api/Controllers/AuthController.cs
@@ -20,9 +20,6 @@ namespace RJM.API.Controllers
         private readonly IMapper mapper;
         private readonly AuthBLL bll;
 
-        // @@@
-        // Test 2
-
         /// <summary>
         /// The constructor of the Auth controller.
         /// </summary>
@@ -38,9 +35,6 @@ namespace RJM.API.Controllers
             this.mapper = mapper;
             this.bll = bll;
         }
-
-        // Test 3 
-        ////////@@@
 
         [HttpPost]
         [Route("login")]
