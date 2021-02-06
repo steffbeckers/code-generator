f8d85a12b1b1ac7532da3b862c1e4964253f28cf
Steff Beckers <steff@steffbeckers.eu>
2021-02-06 13:42:47 +0100

    Save before generating projects

---
+++ dotnet/CodeGen/_Output/Projects/WebAPI/CodeGenOutput.API/ViewModels/ContactVM.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CodeGenOutput.ViewModels
+{
+    public class ContactVM
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+    }
+}
