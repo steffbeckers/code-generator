fc2e68e5f61ad0ee45c2199b8509ca8117f663e7
Steff Beckers <steff@steffbeckers.eu>
2021-02-06 16:12:40 +0100

    Save before regenerating project template WebAPI

---
+++ dotnet/CodeGen/_Output/Projects/WebAPI/CodeGenOutput.API/BLL/AccountBLL.cs
@@ -0,0 +1,58 @@
+using CodeGenOutput.API.DAL;
+using CodeGenOutput.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CodeGenOutput.API.BLL
+{
+    public interface IAccountBLL
+    {
+        Task<IEnumerable<Account>> GetAccountsAsync();
+        Task<Account> GetAccountByIdAsync(Guid id);
+        Task<IEnumerable<Account>> SearchAccountAsync(string term);
+        Task<Account> CreateAccountAsync(Account account);
+        Task<Account> UpdateAccountAsync(Account account);
+        Task DeleteAccountAsync(Account account);
+    }
+
+    public partial class BusinessLogicLayer : IAccountBLL
+    {
+        private readonly IRepository<Account> _accountRepository;
+
+        public async Task<IEnumerable<Account>> GetAccountsAsync()
+        {
+            return await _accountRepository.GetAsync();
+        }
+
+        public async Task<Account> GetAccountByIdAsync(Guid id)
+        {
+            return await _accountRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Account>> SearchAccountAsync(string term)
+        {
+            return await _accountRepository.SearchAccount(term);
+        }
+
+        public async Task<Account> CreateAccountAsync(Account account)
+        {
+            Account createdAccount = await _accountRepository.CreateAsync(account);
+            await _unitOfWork.Commit();
+            return createdAccount;
+        }
+
+        public async Task<Account> UpdateAccountAsync(Account account)
+        {
+            Account updatedAccount = await _accountRepository.UpdateAsync(account);
+            await _unitOfWork.Commit();
+            return updatedAccount;
+        }
+
+        public async Task DeleteAccountAsync(Account account)
+        {
+            await _accountRepository.DeleteAsync(account);
+            await _unitOfWork.Commit();
+        }
+    }
+}
