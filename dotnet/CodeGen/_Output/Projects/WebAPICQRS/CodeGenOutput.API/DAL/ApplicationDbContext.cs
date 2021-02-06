c70d049ccbbc511048510c548cd05f97aab3e209
Steff Beckers <steff@steffbeckers.eu>
2021-02-06 15:34:22 +0100

    Save before generating projects

---
+++ dotnet/CodeGen/_Output/Projects/WebAPICQRS/CodeGenOutput.API/DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using CodeGenOutput.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeGenOutput.API.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
