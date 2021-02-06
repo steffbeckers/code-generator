5bebbbf0f322b10f2331df8773fb2edaa9d95a5f
Steff Beckers <steff@steffbeckers.eu>
2021-02-06 16:22:47 +0100

    Save before regenerating project template WebAPI

---
+++ dotnet/CodeGen/_Output/Projects/WebAPI/CodeGenOutput.API/DAL/Repositories/AccountRepository.cs
@@ -0,0 +1,18 @@
+using CodeGenOutput.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// TODO: namespace CodeGenOutput.API.DAL.Repositories
+namespace CodeGenOutput.API.DAL
+{
+    public static class AccountRepositoryExtensions
+    {
+        public static async Task<IEnumerable<Account>> SearchAccount(
+            this IRepository<Account> repository,
+            string term
+        )
+        {
+            return await repository.GetAsync(x => x.Name.Contains(term));
+        }
+    }
+}
