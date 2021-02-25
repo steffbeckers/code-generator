ef700d0cd33cf2876eb8dca9f5bd14b5cdc7c6f6
Steff Beckers <steff@steffbeckers.eu>
2021-02-25 19:49:27 +0100

    Angular project update

--- CodeGen/Runners/AngularProjectRunner.cs
+++ CodeGen/Runners/AngularProjectRunner.cs
@@ -33,7 +33,7 @@ namespace CodeGen.Runners
 
             // Start project
             ProcessStartInfo angularRun = new ProcessStartInfo(_configService.AppSettings.GetValue<string>("Paths:NPM"));
-            angularRun.Arguments = @"start -- --host 0.0.0.0 --port 5001 --disable-host-check";
+            angularRun.Arguments = @"start -- --host 0.0.0.0 --disable-host-check";
             angularRun.WorkingDirectory = _outputProjectPath;
             await Process.Start(angularRun).WaitForExitAsync();
         }
