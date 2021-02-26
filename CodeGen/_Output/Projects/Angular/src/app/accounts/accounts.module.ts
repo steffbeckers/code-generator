cc5009448074aa391acc4a8b7cbf7c032f3c584d
Steff Beckers <steff@steffbeckers.eu>
2021-02-27 00:16:32 +0100

    Use shared module

--- CodeGen/_Output/Projects/Angular/src/app/accounts/accounts.module.ts
+++ CodeGen/_Output/Projects/Angular/src/app/accounts/accounts.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { AccountsComponent } from './accounts.component';
 
 @NgModule({
   declarations: [AccountsComponent],
-  imports: [CommonModule, AccountsRoutingModule],
+  imports: [SharedModule, AccountsRoutingModule],
 })
 export class AccountsModule {}
--- CodeGen/_Output/Projects/Angular/src/app/contacts/contacts.module.ts
+++ CodeGen/_Output/Projects/Angular/src/app/contacts/contacts.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts.component';
 
 @NgModule({
   declarations: [ContactsComponent],
-  imports: [CommonModule, ContactsRoutingModule],
+  imports: [SharedModule, ContactsRoutingModule],
 })
 export class ContactsModule {}
